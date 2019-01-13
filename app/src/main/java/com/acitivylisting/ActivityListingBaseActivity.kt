7f5b4d23b48a063dc9c4b7b62f23c3ded91fc647
Rahul Ravindran <rahul.r945@gmail.com>
2019-01-14 01:49:30 +0530

    - bottom navigation for ActivityListingBaseActivity - Fragments involved without adding to back stack - UI related to activity_base_listing.xml - UI related drawables added

---
+++ app/src/main/java/com/acitivylisting/ActivityListingBaseActivity.kt
@@ -0,0 +1,53 @@
+package com.acitivylisting
+
+import android.os.Bundle
+import androidx.navigation.Navigator
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.redbussearch.common.basecommons.BaseActivity
+import kotlinx.android.synthetic.main.activity_base_listing.*
+/**
+ * @Author rahulravindran
+ */
+class ActivityListingBaseActivity: BaseActivity(), Navigator.OnNavigatorBackPressListener{
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_base_listing)
+
+        setupNavController()
+    }
+
+    private fun setupNavController() {
+        val hostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        NavigationUI.setupWithNavController(bottom_nav_frag, hostFragment.navController)
+        bottom_nav_frag.setOnNavigationItemSelectedListener { item ->
+            when(item.itemId) {
+                R.id.activitites_frag -> {
+                    hostFragment.navController.navigate(R.id.activitites_frag)
+                    true
+                }
+                R.id.eat_out_frag -> {
+                    hostFragment.navController.navigate(R.id.eat_out_frag)
+                    true
+                }
+                R.id.events_frag -> {
+                    hostFragment.navController.navigate(R.id.events_frag)
+                    true
+                }
+                R.id.you_frag -> {
+                    hostFragment.navController.navigate(R.id.you_frag)
+                    true
+                }
+                else -> false
+            }
+        }
+    }
+
+
+    override fun onPopBackStack(navigator: Navigator<*>) {
+        // kill activity if backpressed
+        finish()
+    }
+}
---
+++ app/src/main/java/com/acitivylisting/fragments/YouFragment.kt
@@ -0,0 +1,9 @@
+package com.acitivylisting.fragments
+
+import com.redbussearch.common.basecommons.BaseFragment
+
+/**
+ * @Author rahulravindran
+ */
+class YouFragment: BaseFragment() {
+}
